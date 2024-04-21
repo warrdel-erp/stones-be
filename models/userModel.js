24ef781f3965bbb92355aaff73eb9d80c48f363b
siddharth-warrdel <siddharth@warrdel.com>
2024-04-21 18:16:16 +0530

    time stamp fix

--- models/userModel.js
+++ models/userModel.js
@@ -30,19 +30,19 @@ export default sequelize.define(
         allowNull: false
     },
     createdAt: {
-        type: DataTypes.TIME,
+        type: DataTypes.DATE,
         allowNull: false,
         defaultValue: sequelize.literal('CURRENT_TIMESTAMP'),
         field:'created_at'
     },
     updatedAt: {
-        type: DataTypes.TIME,
+        type: DataTypes.DATE,
         allowNull: false,
         defaultValue: sequelize.literal('CURRENT_TIMESTAMP'),
         field:'updated_at'
     },
     deletedAt: {
-        type: DataTypes.TIME,
+        type: DataTypes.DATE,
         allowNull: false,
         defaultValue: sequelize.literal('CURRENT_TIMESTAMP'),
         field:'deleted_at'
