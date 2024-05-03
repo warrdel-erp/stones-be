1baae6a1f8848263afe1343c97db7c3665af4047
Kuldeepyadav0101 <kuldeep.yadav@warrdel.com>
2024-05-03 10:15:05 +0530

    WSERP-20 changes

--- controllers/supplierController.js
+++ controllers/supplierController.js
@@ -25,7 +25,7 @@ export const getAllSupplier = async (req,res) => {
 
 // 3. get single supplier details
 export const getSingleSupplierDetails = async (req,res) => {
-    const supplierName =req.query.supplierName;
+    const supplierName = req.query.supplierName;
     try {
         if (!supplierName){
             res.status(400).send("supplierName is required");
