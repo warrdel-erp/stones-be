bf7e12de6822ea9196e843b14e8d7b3ed56b3aa7
siddharth-warrdel <siddharth@warrdel.com>
2024-07-11 19:00:21 +0530

    drop supplierId from products

--- models/productModel.js
+++ models/productModel.js
@@ -197,15 +197,6 @@ export default sequelize.define(
         allowNull: true,
         field:'deleted_at',
     },
-    supplierId: {
-        type: DataTypes.INTEGER,
-        allowNull: false,
-        field: 'supplier_id',
-        references: {
-            model: Supplier,
-            key: 'supplier_id'
-        }
-    },
     status: {
         type: DataTypes.ENUM(...status),
         allowNull: false,
