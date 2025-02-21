276a673d2c3f5b297f41beb2c2ce521c6625a142
name <jitin.kumar@warrdel.com>
2025-02-22 01:48:32 +0530

    update po with products.

--- repository/prePurchaseRepository.js
+++ repository/prePurchaseRepository.js
@@ -1,14 +1,15 @@
+import { array } from "zod";
 import * as model from "../models/index.js";
 // import { Op } from "sequelize";
 
-export async function getPrePurchaseProductDetails(prePurchaseOrderId) {  
+export async function getPrePurchaseProductDetails(prePurchaseOrderId) {
   try {
     const result = await model.prePurchaseModel.findOne({
       where: {
         prePurchaseOrderId: prePurchaseOrderId
       }
     });
-    
+
     return result;
   } catch (error) {
     console.error("Error in fetching pre-purchase product details:", error);
@@ -20,12 +21,31 @@ export async function getPrePurchaseProductDetails(prePurchaseOrderId) {
 //update prepurchase invoice product details
 export async function updatePrePurchaseProductDetails(data) {
   try {
-    const result = await model.prePurchaseModel.update(data, {
-      where: {
-        prePurchaseOrderId: data.prePurchaseOrderId
-      }
-    });
-    return result;
+
+    let result;
+
+    if (Array.isArray(data)) {
+      data.forEach(async e => {
+        result = await model.prePurchaseModel.update(e, {
+          where: {
+            prePurchaseOrderId: e.prePurchaseOrderId
+          }
+        });
+        console.log(result)
+      })
+      // result = model.prePurchaseModel.bulkCreate(data, { updateOnDuplicate: ["prePurchaseOrderId"] })
+    } else {
+
+      result = await model.prePurchaseModel.update(data, {
+        where: {
+          prePurchaseOrderId: data.prePurchaseOrderId
+        }
+      });
+    }
+
+
+
+    return {result: "products updated successfully"};
   } catch (error) {
     console.error("Error in updating pre-purchase product details:", error);
     throw error;
