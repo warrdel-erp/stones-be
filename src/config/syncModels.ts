030c6f20e3afb3c504413cb1ec528abe26e4f501
Jitin <jkgoyal85@gmail.com>
2025-03-03 10:23:29 +0530

    sync models

--- src/config/syncModels.ts
+++ src/config/syncModels.ts
@@ -1,8 +1,11 @@
 // these unused imports are not redundant, They are useful to sync models
-import { ProductCategory } from "../models";
+import * as models from "../models";
 import { sequelize } from "./database";
 
-export const syncModels = () => {
-  ProductCategory.sync({ alter: true });
+export const syncModels = async () => {
+  await models.Client.sync({ alter: true });
+  await models.Location.sync({ alter: true });
+  await models.ProductCategory.sync({ alter: true });
+  await models.User.sync({ alter: true });
   sequelize.sync({ alter: true });
 };
