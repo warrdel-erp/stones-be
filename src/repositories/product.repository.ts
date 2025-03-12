1b5110145231c8dd4c8d205a2ce464573fda63ee
Jitin <jkgoyal85@gmail.com>
2025-03-12 17:03:52 +0530

    category

--- src/repositories/product.repository.ts
+++ src/repositories/product.repository.ts
@@ -39,10 +39,7 @@ export const getProductByIdSimple = async (id: number) => {
 export const getProductById = async (id: number) => {
   return await models.Product.findOne({
     where: { id },
-    include: [
-      { model: models.Slab, as: "slabs" },
-      { model: models.ProductCategory, as: "category" },
-    ],
+    include: [{ model: models.Slab, as: "slabs" }],
   });
 };
 
