0abf9891cccbea6ab7ec6674eb7eae640a1550e1
Jitin <jkgoyal85@gmail.com>
2025-03-26 15:25:39 +0530

    refactor fetchProductsWithSlabsByLocation to use let for finalData and filter out undefined values

--- src/services/inventory.service.ts
+++ src/services/inventory.service.ts
@@ -7,7 +7,7 @@ import * as slabRepository from "../repositories/slab.repository";
 export const fetchProductsWithSlabsByLocation = async (page: number, limit: number, locationId: number) => {
   const data: any = await productRepository.getAllProducts(page, limit);
 
-  const finalData = await Promise.all(
+  let finalData = await Promise.all(
     data.products.map(async (product: any) => {
       product = product.get({ plain: true });
 
@@ -32,6 +32,8 @@ export const fetchProductsWithSlabsByLocation = async (page: number, limit: numb
       return product;
     })
   );
+
+  finalData = finalData.filter((e) => e);
 
   return { products: finalData, total: data.total };
 };
