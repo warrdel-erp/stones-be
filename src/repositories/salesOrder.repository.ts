a22e597d71b8dc6b78ec218348848988a01ec60d
Jitin <jkgoyal85@gmail.com>
2025-03-28 13:32:43 +0530

    loadingORderData

--- src/repositories/salesOrder.repository.ts
+++ src/repositories/salesOrder.repository.ts
@@ -33,7 +33,16 @@ export const getSalesOrderById = async (id: number) => {
       { model: models.Customer, as: "customer" },
       { model: models.User, as: "createdBy" },
       { model: models.CustomerAddress, as: "shippingAddress" },
-      { model: models.LoadingOrder, as: "loadingOrders" },
+      {
+        model: models.LoadingOrder,
+        as: "loadingOrders",
+        include: [
+          {
+            model: models.LoadingOrderProduct,
+            as: "loadingOrderProducts",
+          },
+        ],
+      },
       {
         model: models.SalesOrderProduct,
         as: "salesOrderProducts",
