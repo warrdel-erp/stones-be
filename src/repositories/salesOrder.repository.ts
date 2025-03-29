acb82e5f7d0ae21f62d02405bc3aedcab804e83e
Jitin <jkgoyal85@gmail.com>
2025-03-29 16:15:27 +0530

    notes in SO

--- src/repositories/salesOrder.repository.ts
+++ src/repositories/salesOrder.repository.ts
@@ -33,6 +33,7 @@ export const getSalesOrderById = async (id: number) => {
       { model: models.Customer, as: "customer" },
       { model: models.User, as: "createdBy" },
       { model: models.CustomerAddress, as: "shippingAddress" },
+      { model: models.Notes, as: "notes" },
       {
         model: models.SalesOrderProduct,
         as: "salesOrderProducts",
