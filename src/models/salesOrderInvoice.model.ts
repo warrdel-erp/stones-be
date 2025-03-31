812f62eac8bf54cf1c0df5331bb0a69995eef7cb
Jitin <jkgoyal85@gmail.com>
2025-03-31 14:43:13 +0530

    clientSoInvoiceNumber validations

--- src/models/salesOrderInvoice.model.ts
+++ src/models/salesOrderInvoice.model.ts
@@ -5,76 +5,82 @@ import LoadingOrder from "./loadingOrder.model";
 import Client from "./client";
 
 export type SoInvoice = {
-    customerId: number;
-    loadingOrderId: number;
-    clientId: number;
-    amount: number;
-}
+  customerId: number;
+  loadingOrderId: number;
+  clientId: number;
+  amount: number;
+};
 
 const SalesOrderInvoice = sequelize.define(
-    "SalesOrderInvoice",
-    {
-        id: {
-            type: DataTypes.INTEGER,
-            autoIncrement: true,
-            primaryKey: true,
-        },
-        amount: {
-            type: DataTypes.FLOAT,
-            allowNull: false,
-        },
-        clientSoInvoiceNumber: {
-            type: DataTypes.STRING,
-            allowNull: false,
-            unique: true,
-        },
-        customerId: {
-            type: DataTypes.INTEGER,
-            references: {
-                model: Customer,
-                key: "id",
-            },
-            onUpdate: "CASCADE",
-            allowNull: false,
-        },
-        loadingOrderId: {
-            type: DataTypes.INTEGER,
-            references: {
-                model: LoadingOrder,
-                key: "id",
-            },
-            onUpdate: "CASCADE",
-            allowNull: false,
-        },
-        clientId: {
-            type: DataTypes.INTEGER,
-            references: {
-                model: Client,
-                key: "id",
-            },
-            onUpdate: "CASCADE",
-            onDelete: "RESTRICT",
-        },
+  "SalesOrderInvoice",
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      autoIncrement: true,
+      primaryKey: true,
     },
-    {
-        tableName: "sales_order_invoices",
-        timestamps: true, // Enables createdAt and updatedAt fields
-    }
+    amount: {
+      type: DataTypes.FLOAT,
+      allowNull: false,
+    },
+    clientSoInvoiceNumber: {
+      type: DataTypes.STRING,
+      allowNull: true,
+    },
+    customerId: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: Customer,
+        key: "id",
+      },
+      onUpdate: "CASCADE",
+      allowNull: false,
+    },
+    loadingOrderId: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: LoadingOrder,
+        key: "id",
+      },
+      onUpdate: "CASCADE",
+      allowNull: false,
+    },
+    clientId: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: Client,
+        key: "id",
+      },
+      onUpdate: "CASCADE",
+      onDelete: "RESTRICT",
+    },
+  },
+  {
+    tableName: "sales_order_invoices",
+    timestamps: true, // Enables createdAt and updatedAt fields
+    indexes: [
+      {
+        unique: true,
+        fields: ["clientId", "clientSoInvoiceNumber"],
+      },
+    ],
+  }
 );
 
 // 🔹 Hook: Auto-Increment `clientInvoiceNumber` based on `clientId`
 SalesOrderInvoice.beforeCreate(async (soInvoice: any) => {
-    if (!soInvoice.clientId && !soInvoice.purchaseOrderId) {
-        throw new Error("Client ID is required to generate clientSoInvoiceNumber.");
-    }
+  if (!soInvoice.clientId && !soInvoice.purchaseOrderId) {
+    throw new Error("Client ID is required to generate clientSoInvoiceNumber.");
+  }
 
-    const lastSoInvoiceAccordingToClient: any = await SalesOrderInvoice.findOne({
-        where: { clientId: soInvoice.clientId },
-        order: [["clientSoInvoiceNumber", "DESC"]],
-    });
+  const lastSoInvoiceAccordingToClient: any = await SalesOrderInvoice.findOne({
+    where: { clientId: soInvoice.clientId },
+    order: [["clientSoInvoiceNumber", "DESC"]],
+  });
 
-    soInvoice.clientSoInvoiceNumber = lastSoInvoiceAccordingToClient ? lastSoInvoiceAccordingToClient.clientSoInvoiceNumber + 1 : 1;
+  soInvoice.clientSoInvoiceNumber = lastSoInvoiceAccordingToClient
+    ? lastSoInvoiceAccordingToClient.clientSoInvoiceNumber + 1
+    : 1;
 });
-
 
 export default SalesOrderInvoice;
