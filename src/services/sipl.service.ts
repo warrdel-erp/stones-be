61b78d8407aa6a62ab2c8e85c90296d56fb774e6
jitin <jitin.goyal@gmail.com>
2025-12-01 15:03:55 +0530

    division zero

--- src/services/sipl.service.ts
+++ src/services/sipl.service.ts
@@ -306,7 +306,7 @@ export const getSIPLById = async (id: number) => {
   sipl.siplProducts = sipl.siplProducts.map((siplProduct: any) => {
 
     const totalReceivedQuantity = sumDecimal(siplProduct.slabs, "receivedSqrFt")
-    const itemUnitCost = decimalDivide(siplProduct.totalCost, totalReceivedQuantity)
+    const itemUnitCost = totalReceivedQuantity && decimalDivide(siplProduct.totalCost, totalReceivedQuantity)
 
     return {
       ...siplProduct,
