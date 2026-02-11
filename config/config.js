f54e0b15906941cc138fd857720e7a328eb20815
jitin <jitin.goyal@gmail.com>
2026-02-12 01:17:17 +0530

    db config for port

--- config/config.js
+++ config/config.js
@@ -2,10 +2,11 @@ require("dotenv").config();
 
 module.exports = {
   development: {
-    username: process.env.DB_USER || "root",
-    password: process.env.DB_PASSWORD || "password",
-    database: process.env.DB_NAME || "database_name",
-    host: process.env.DB_HOST || "127.0.0.1",
+    username: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT || 3306,
     dialect: "mysql",
   },
   production: {
@@ -13,6 +14,7 @@ module.exports = {
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
     host: process.env.DB_HOST,
+    port: process.env.DB_PORT || 3306,
     dialect: "mysql",
   },
 };
